01fac734867e922761639ba54ccc97f34d14178f
Your Name <liemhoanglong@gmail.com>
2019-12-15 23:14:39 +0700

    done add, edit and delete features of category and publisher

--- routes/product.js
+++ routes/product.js
@@ -22,6 +22,70 @@ router.get('/add-product', controllerProduct.addProduct);
 /* GET edit-product page. */
 router.get('/edit-product', controllerProduct.editProduct);
 
+/* POST insert-category page. */
+router.post('/insert-category', (req, res, next) => {
+const temp = {
+		categoriesID: req.body.code,
+		categories: req.body.name
+	}
+	let data = new categories(temp);
+	console.log(data);
+	data.save();
+	res.redirect('/products');
+});
+
+/* POST insert-publisher page. */
+router.post('/insert-publisher', (req, res, next) => {
+	const temp = {
+		publisherID: req.body.code,
+		publisher: req.body.name
+	}
+	let data = new publishers(temp);
+	console.log(data);
+	data.save();
+	res.redirect('/products');
+});
+
+/* POST edit-category page. */
+router.post('/edit-category', (req, res, next) => {
+	//console.log(req.query.id);
+	categories.findById(req.query.id)
+	.then (data => {
+		//console.log(req.body.code);
+		//console.log(req.body.name);
+		//console.log(data);
+		data.categoriesID = req.body.code;
+		data.categories = req.body.name;
+		data.save();
+	})
+	res.redirect('/products');
+});
+
+/* POST edit-publisher page. */
+router.post('/edit-publisher', (req, res, next) => {
+	//console.log(req.query.id);
+	publishers.findById(req.query.id)
+	.then (data => {
+		data.publisherID = req.body.code;
+		data.publisher = req.body.name;
+		data.save();
+	})
+	res.redirect('/products');
+});
+
+/* GET category delete page. */
+router.get('/category-delete', function (req, res, next) {
+	categories.findByIdAndRemove(req.query.id, (err) => {
+		res.redirect('/products');
+	});
+});
+
+/* GET publisher delete page. */
+router.get('/publisher-delete', function (req, res, next) {
+	publishers.findByIdAndRemove(req.query.id, (err) => {
+		res.redirect('/products');
+	});
+});
 
 /* GET product delete page. */
 router.get('/product-delete', function (req, res, next) {
